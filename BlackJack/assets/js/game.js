d486872b56ba6dd04fbafe59e27e26021041d4c1
Angel <gutierrezramosjoseangel@gmail.com>
2020-12-15 20:41:56 -0600

    Module  Pattern

--- BlackJack/assets/js/game.js
+++ BlackJack/assets/js/game.js
@@ -1,156 +1,152 @@
-/*
-    2C = Two of clubs (Tréboles)
-    2D = Two of diamonds (Diamantes)
-    2H = Two of hearts (Corazones)
-    2S = Two of spades (Espadas)
- */
+//Patron modulo, no se pueden llamar en objeto global windows
+(() => {
+    'use strict'
+    //General variables
+    let deck = [];
+    const types = ['C', 'D', 'H', 'S'];
+    const specials = ['A', 'J', 'Q', 'K'];
 
- //General variables
- let deck = [];
- const types = ['C', 'D', 'H', 'S'];
- const specials = ['A', 'J', 'Q', 'K'];
+    let playerPoints = 0;
+    let AIPoints = 0;
 
- let playerPoints = 0;
- let AIPoints = 0;
+    //References of DOM
+    const btnAskForCard = document.querySelector('#btnAskForCard');
+    const btnStop = document.querySelector('#btnStop');
+    const btnNewGame = document.querySelector('#btnNewGame');
 
- //References of DOM
- const btnAskForCard = document.querySelector('#btnAskForCard');
- const btnStop = document.querySelector('#btnStop');
- const btnNewGame = document.querySelector('#btnNewGame');
- 
- const scorePoints = document.querySelectorAll('small');
- const playerCards =  document.querySelector('#player-cards');
- const AICards =  document.querySelector('#AI-cards');
+    const scorePoints = document.querySelectorAll('small');
+    const playerCards = document.querySelector('#player-cards');
+    const AICards = document.querySelector('#AI-cards');
 
 
- const createDeck = () => {
+    const createDeck = () => {
 
-    //All cards with numbers
-    for (let i = 2; i <= 10; i++) {
+        //All cards with numbers
+        for (let i = 2; i <= 10; i++) {
+            for (const type of types) {
+                deck.push(i + type);
+            }
+        }
+
+        //Aces
         for (const type of types) {
-            deck.push(i + type);
+            for (const special of specials) {
+                deck.push(special + type);
+            }
         }
+
+        // console.log(deck);
+        deck = _.shuffle(deck);
+        return deck;
     }
 
-    //Aces
-    for (const type of types) {
-        for (const special of specials) {
-            deck.push(special + type);
+    createDeck();
+
+    const askForCard = () => {
+        if (deck.length === 0) {
+            throw 'No card in deck';
+        } else {
+
         }
+
+        const card = deck.pop();
+        // console.log(deck);
+        // console.log(card); ///The card must be in the deck
+        return card;
     }
 
-    // console.log(deck);
-    deck = _.shuffle(deck);
-    console.log(deck);
-    return deck;
- }
+    //  askForCard();
 
- createDeck();
-
- const askForCard = () => {
-    if(deck.length === 0) {
-        throw 'No card in deck';
-    } else {
-
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1);
+        return (isNaN(value)) ?
+            (value === 'A') ? 11 : 10
+            : value * 1;
     }
 
-    const card = deck.pop();
-    // console.log(deck);
-    // console.log(card); ///The card must be in the deck
-    return card;
- }
+    //Computer turn
+    const computerTurn = (minimumPoints) => {
 
-//  askForCard();
+        do {
+            const card = askForCard();
 
- const cardValue = (card) => {
-    const value = card.substring(0,card.length - 1);
-    return (isNaN(value)) ?
-        (value === 'A') ? 11 : 10
-        : value * 1;
- }
+            AIPoints += cardValue(card);
+            scorePoints[1].innerText = AIPoints;
 
- //Computer turn
- const computerTurn = (minimumPoints) => {
+            const imgCard = document.createElement('img');
+            imgCard.src = `assets/cartas/${card}.png`;
+            imgCard.classList.add('BJ-card');
+            AICards.append(imgCard);
 
-    do {
+            if (minimumPoints > 21) {
+                break;
+            }
+
+        } while ((AIPoints < minimumPoints) && (minimumPoints <= 21));
+
+        setTimeout(() => {
+            if (AIPoints === minimumPoints) {
+                alert('Draw');
+            } else if (minimumPoints > 21) {
+                alert('AI Wins');
+            } else if (AIPoints > 21) {
+                alert('Player Wins');
+            } else if (AIPoints > minimumPoints) {
+                alert('AI Wins');
+            }
+        }, 100);
+    }
+
+    //Events
+    btnAskForCard.addEventListener('click', () => {
         const card = askForCard();
-        
-        AIPoints += cardValue(card);
-        scorePoints[1].innerText = AIPoints;
+        playerPoints += cardValue(card);
+        scorePoints[0].innerText = playerPoints;
 
         const imgCard = document.createElement('img');
         imgCard.src = `assets/cartas/${card}.png`;
         imgCard.classList.add('BJ-card');
-        AICards.append(imgCard);
+        playerCards.append(imgCard);
 
-        if(minimumPoints > 21) {
-            break;
+        if (playerPoints > 21) {
+            // alert('Perdiste');
+            btnStop.disabled = true;
+            btnAskForCard.disabled = true;
+            computerTurn(playerPoints);
+
+        } else if (playerPoints === 21) {
+            // alert('Ganaste');
+            btnStop.disabled = true;
+            btnAskForCard.disabled = true;
+            computerTurn(playerPoints);
         }
 
-    } while ( (AIPoints < minimumPoints) && (minimumPoints <= 21) );
+    });
 
-    setTimeout(() => {
-        if( AIPoints === minimumPoints) {
-            alert('Draw');
-        } else if( minimumPoints > 21) {
-            alert('AI Wins');
-        } else if( AIPoints > 21 ){ 
-            alert('Player Wins');
-        } else {
-            alert('AI Wins');
-        }
-    }, 100);
- }
 
-//Events
-btnAskForCard.addEventListener('click', () => {
-    const card = askForCard();
-    playerPoints += cardValue(card);
-    scorePoints[0].innerText = playerPoints;
-
-    const imgCard = document.createElement('img');
-    imgCard.src = `assets/cartas/${card}.png`;
-    imgCard.classList.add('BJ-card');
-    playerCards.append(imgCard);
-
-     if(playerPoints > 21) {
-        // alert('Perdiste');
+    btnStop.addEventListener('click', () => {
         btnStop.disabled = true;
         btnAskForCard.disabled = true;
         computerTurn(playerPoints);
-
-     } else if ( playerPoints === 21) {
-        // alert('Ganaste');
-        btnStop.disabled = true;
-        btnAskForCard.disabled = true;
-        computerTurn( playerPoints );
-     }
-
-});
+    });
 
 
-btnStop.addEventListener('click', () => {
-    btnStop.disabled = true;
-    btnAskForCard.disabled = true;
-    computerTurn( playerPoints );
-});
+    btnNewGame.addEventListener('click', () => {
 
+        console.clear();
+        deck = [];
+        deck = createDeck();
 
-btnNewGame.addEventListener('click', () => {
+        playerPoints = 0;
+        AIPoints = 0;
+        scorePoints[0].innerText = 0;
+        scorePoints[1].innerText = 0;
 
-    console.clear();
-    deck = [];
-    deck = createDeck();
+        playerCards.innerHTML = '';
+        AICards.innerHTML = '';
 
-    playerPoints = 0;
-    AIPoints = 0;
-    scorePoints[0].innerText = 0;
-    scorePoints[1].innerText = 0;
+        btnStop.disabled = false;
+        btnAskForCard.disabled = false;
 
-    playerCards.innerHTML = '';
-    AICards.innerHTML = '';
-
-    btnStop.disabled = false;
-    btnAskForCard.disabled = false;
-
-});
+    });
+})();
